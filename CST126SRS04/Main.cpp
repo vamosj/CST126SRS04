e6de68aa65f7aef404dda2e069eb0311da9a63c6
vamosj <janos.vamos@oit.edu>
2018-03-13 21:10:49 -0700

    Setup
    Set Up Program

---
+++ CST126SRS04/Main.cpp
@@ -0,0 +1,26 @@
+// CST126SRS04.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+class CSWoman
+{
+private:
+	const std::string name_;
+	const unsigned birthYear_;
+	const std::string fact_;
+
+public:
+	CSWoman(const char* const name, const unsigned birthYear, const char* const fact);
+
+	// ... and other methods  
+};
+
+
+
+
+int main()
+{
+    return 0;
+}
+
